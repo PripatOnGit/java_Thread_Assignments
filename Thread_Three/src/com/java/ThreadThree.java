a2c8bfa1b0b6ad6181b81b8ba25d6358db860038
PripatOnGit <patil.priyu26@gmail.com>
2023-04-14 17:03:05 +0530

    Commit for 3rd Problem statement

---
+++ Thread_Three/src/com/java/ThreadThree.java
@@ -0,0 +1,68 @@
+package com.java;
+
+class ThreadOne extends Thread {
+	synchronized static void printSeries() {
+		
+			for(int i=1;i<=10;i++) {
+				if(i==1) {
+					System.out.print("1 ");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}else
+					{System.out.print("1/"+i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				} 
+			}
+		}
+	
+	public void run() {
+		 printSeries();
+	}
+}
+
+class ThreadTwo extends Thread {
+	synchronized static void printSeries() {
+		for(int i=1;i<=10;i++) {
+			if(i==1) {
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					}
+				}
+			else{
+				System.out.print("! + ");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+			} 
+		}
+	}
+	public void run() {
+		 printSeries();
+	}
+}
+public class ThreadThree extends Thread {
+	public static void main(String[] args) {
+		ThreadOne t1 = new ThreadOne();
+		ThreadTwo t2 = new ThreadTwo();
+		
+		t1.start();
+		t2.start();
+	}
+
+}
---
+++ Thread_Three/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author priya
+ *
+ */
+module Thread_Three {
+}
